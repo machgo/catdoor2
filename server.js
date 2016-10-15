0587b50adeca6b5be17e0a4c22fd01763d8fe921
Marco Schmid <marco@marcoschmid.me>
2016-10-16 00:02:39 +0200

    enable cors

--- server.js
+++ server.js
@@ -8,6 +8,13 @@ var app = express();
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://mongo/catdoor-prod");
 
+//enable CORS
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use(bodyParser.urlencoded({
     extended: true
 }));
